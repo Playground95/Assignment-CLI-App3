1398087964103beabceec1dedc629fdc66d40f23
nadeesha <nadeeshalakshan525@gmail.com>
2023-08-16 07:17:49 +0530

    Created Dashboard

--- App3.java
+++ App3.java
@@ -18,6 +18,44 @@ public class App3{
         final String TRANSFERS = "Transfers";
         final String CHECK_ACCOUNT_BALANCE = "Check Account Balance";
         final String DELETE_ACCOUNT = "Delete an Existing Account";
+
+        String screen = DASHBOARD;
+
+
+        do {
+            final String APP_TITLE = String.format("%s%s%s",
+                                COLOR_BLUE_BOLD, screen, RESET);
+
+            System.out.println(CLEAR);
+            System.out.println("\t" + APP_TITLE + "\n");
+
+            switch(screen){
+                case DASHBOARD: 
+                    System.out.println("\t[1]. Create New Account");
+                    System.out.println("\t[2]. Deposits");
+                    System.out.println("\t[3]. Withdraws");
+                    System.out.println("\t[4]. Transfers");
+                    System.out.println("\t[5]. Check Account Balance");
+                    System.out.println("\t[6]. Delete an Existing Account");
+                    System.out.println("\t[7]. Exit\n");
+                    System.out.print("\tEnter an option to continue: ");
+                    int option = SCANNER.nextInt();
+                    SCANNER.nextLine();
+
+                    switch (option){
+                        case 1: screen = CREATE_NEW_ACCOUNT; break;
+                        case 2: screen = DEPOSITS; break;
+                        case 3: screen = WITHDRAWS; break;
+                        case 4: screen = TRANSFERS; break;
+                        case 5: screen = CHECK_ACCOUNT_BALANCE; break;
+                        case 6: screen = DELETE_ACCOUNT; break;
+                        case 7: System.out.println(CLEAR); System.exit(0);
+                        default: continue;
+                    }
+                    break;
+            }
+
+        }while(true);
         
     }
 }
